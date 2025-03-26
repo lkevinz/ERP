113add5e57dcdbcb8361300c6201656dc7bc85c0
Kevin Salinas <kevinsalinas200408@gmail.com>
2025-03-26 21:37:00 +0100

    ERP - BETA

--- src/screens/ConexionSQL.py
+++ src/screens/ConexionSQL.py
@@ -1,17 +1,21 @@
 import pymysql
 
+
 # ABRIR CONEXION
 def conexionDB():
     try:
-        miConexion = pymysql.connect(host='localhost',user='root',passwd='',db='informe_rentabilidades')
+        miConexion = pymysql.connect(
+            host="localhost", user="root", passwd="", db="informe_rentabilidades"
+        )
         cur = miConexion.cursor()
         print("Conexión exitosa a la base de datos")
         return miConexion, cur
     except pymysql.Error as e:
         print(f"Error al conectar a la base de datos: {e}")
         return None, None
-    
-#   CERRAR CONEXION 
+
+
+#   CERRAR CONEXION
 def cerrarConexion(miConexion):
     """Cierra la conexión con la base de datos."""
     if miConexion:
